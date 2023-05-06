e82c6e3fb48b92fbe5086b1a4488ae685cfd8d9b
akiogitgit <akimaron2828@icloud.com>
2023-05-06 23:47:53 +0900

    feat: tiptapで書いたHTMLにスタイルを当てて表示

--- frontend/src/pages/curriculums.tsx
+++ frontend/src/pages/curriculums.tsx
@@ -42,9 +42,146 @@ const Curriculums: NextPage = () => {
           return (
             <li
               key={curriculum.id}
-              className='rounded-md flex border-2 shadow-md mb-3 py-4 px-4 items-center justify-between'
+              // className='rounded-md flex border-2 shadow-md mb-3 py-4 px-4 items-center justify-between'
             >
-              {curriculum.name}
+              <div>
+                {curriculum.name}
+                <div
+                  className='ProseMirror'
+                  dangerouslySetInnerHTML={{
+                    __html: curriculum.articleUrl || '',
+                  }}
+                />
+
+                <style jsx global>{`
+                  .ProseMirror h1 {
+                    margin: 1.5em 0 1em;
+                    font-size: 2em;
+                    font-weight: bold;
+                    line-height: 1.2;
+                  }
+                  .ProseMirror h2 {
+                    margin: 1.5em 0 1em;
+                    font-size: 1.5em;
+                    font-weight: bold;
+                    line-height: 1.2;
+                  }
+                  .ProseMirror h3 {
+                    margin: 1.5em 0 1em;
+                    font-size: 1.17em;
+                    font-weight: bold;
+                    line-height: 1.2;
+                  }
+                  .ProseMirror h4 {
+                    margin: 1.5em 0 1em;
+                    font-size: 1em;
+                    font-weight: bold;
+                    line-height: 1.2;
+                  }
+                  .ProseMirror h5 {
+                    margin: 1.5em 0 1em;
+                    font-size: 0.83em;
+                    font-weight: bold;
+                    line-height: 1.2;
+                  }
+                  .ProseMirror h6 {
+                    margin: 1.5em 0 1em;
+                    font-size: 0.67em;
+                    font-weight: bold;
+                    line-height: 1.2;
+                  }
+
+                  .ProseMirror ul,
+                  .ProseMirror ol {
+                    margin: 0;
+                    padding-left: 20px;
+                  }
+                  .ProseMirror ul {
+                    list-style: disc;
+                  }
+                  .ProseMirror ol {
+                    list-style: decimal;
+                  }
+                  .ProseMirror li {
+                    margin: 0.5em 0;
+                  }
+
+                  .ProseMirror code {
+                    font-family: 'Courier New', Courier, monospace;
+                    background-color: #f5f5f5;
+                    padding: 0.2em 0.4em;
+                    border-radius: 0.2em;
+                    border: 1px solid #ccc;
+                  }
+                  .ProseMirror pre code {
+                    display: block;
+                    font-family: 'Courier New', Courier, monospace;
+                    background-color: #f5f5f5;
+                    padding: 1em;
+                    overflow: auto;
+                    border-radius: 0.2em;
+                    white-space: pre-wrap;
+                  }
+
+                  .ProseMirror a {
+                    color: #228be6;
+                  }
+                  .ProseMirror a:hover {
+                    text-decoration: underline;
+                  }
+
+                  .ProseMirror hr {
+                    border: none;
+                    height: 1px;
+                    background-color: #ccc;
+                    margin: 1em 0;
+                  }
+
+                  .ProseMirror blockquote {
+                    border-left: 4px solid #ccc;
+                    color: #555;
+                    font-style: italic;
+                    margin: 0;
+                    padding: 1em;
+                  }
+                  .ProseMirror blockquote p {
+                    margin: 0;
+                  }
+
+                  .ProseMirror p {
+                    font-size: 16px;
+                    line-height: 1.5;
+                    margin: 1em 0;
+                  }
+
+                  .ProseMirror sup,
+                  .ProseMirror sub {
+                    font-size: 0.8em;
+                    position: relative;
+                    vertical-align: baseline;
+                  }
+                  .ProseMirror sup {
+                    top: -0.5em;
+                  }
+                  .ProseMirror sub {
+                    bottom: -0.25em;
+                  }
+
+                  .ProseMirror strong {
+                    font-weight: bold;
+                  }
+                  .ProseMirror s {
+                    text-decoration: line-through;
+                  }
+                  .ProseMirror em {
+                    font-style: italic;
+                  }
+                  .ProseMirror u {
+                    text-decoration: underline;
+                  }
+                `}</style>
+              </div>
+
               <Flex align='center'>
                 <UpdateCurriculumButton
                   onSubmit={v => updateCurriculum(curriculum.id, v)}
