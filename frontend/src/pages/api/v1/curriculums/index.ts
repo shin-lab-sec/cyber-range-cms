785e79f9843dd8a4ced426618f5638c96df5b0e4
akiogitgit <akimaron2828@icloud.com>
2023-05-07 21:36:04 +0900

    feat: api/v1でGETのみのエンドポイントを作成

---
+++ frontend/src/pages/api/v1/curriculums/index.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import prisma from '@/libs/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const curriculums = await prisma.curriculum.findMany({
+      orderBy: { createdAt: 'asc' },
+    })
+    res.status(200).json({ data: curriculums })
+  } catch (err) {
+    res.status(400).json({ data: err })
+  }
+}
