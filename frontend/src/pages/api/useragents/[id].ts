7210f85359cd5b2703983208f16376f2f883a63e
akiogitgit <akimaron2828@icloud.com>
2023-05-11 14:49:07 +0900

    feat: userAgentのAPI作成

---
+++ frontend/src/pages/api/useragents/[id].ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import prisma from '@/libs/prisma'
+import { apiValidation, userAgentSchema } from '@/libs/validates'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { method, body } = req
+  const id = String(req.query.id)
+
+  // useragents/[id]
+  switch (method) {
+    case 'GET':
+      try {
+        const userAgent = await prisma.userAgent.findUnique({
+          where: {
+            id: id,
+          },
+        })
+        res.status(200).json({ data: userAgent })
+      } catch (err) {
+        res.status(400).json({ data: err })
+      }
+      break
+
+    case 'PUT':
+      apiValidation(req, res, userAgentSchema, async () => {
+        const updatedUserAgent = await prisma.userAgent.update({
+          where: {
+            id: id,
+          },
+          data: {
+            name: body.name,
+            gitHubUrl: body.gitHubUrl,
+          },
+        })
+        res.status(200).json({ data: updatedUserAgent })
+      })
+      break
+
+    case 'DELETE':
+      try {
+        const deletedUserAgent = await prisma.userAgent.delete({
+          where: {
+            id: id,
+          },
+        })
+        res.status(200).json({ data: deletedUserAgent })
+      } catch (err) {
+        res.status(400).json({ data: err })
+      }
+      break
+
+    default:
+      res.status(405).end()
+      break
+  }
+}
