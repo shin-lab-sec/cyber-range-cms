75e00832299f7e81e428a9b75aa2217bdda6ca1f
akiogitgit <akimaron2828@icloud.com>
2023-05-11 14:51:09 +0900

    feat: userAgentのフォームコンポーネント作成

---
+++ frontend/src/features/userAgent/components/CreateUserAgentButton.tsx
@@ -0,0 +1,35 @@
+import { Button, Modal } from '@mantine/core'
+import { FC } from 'react'
+
+import { useBoolean } from '@/hooks/useBoolean'
+
+import { UserAgentFormRequest, UserAgentForm } from './UserAgentForm'
+
+type Props = {
+  onSubmit: (params: UserAgentFormRequest) => void
+}
+
+export const CreateUserAgentButton: FC<Props> = ({ onSubmit }) => {
+  const isOpen = useBoolean()
+
+  return (
+    <>
+      <Button onClick={isOpen.setTrue}>新規カリキュラム作成</Button>
+      <Modal
+        opened={isOpen.v}
+        onClose={isOpen.setFalse}
+        title='新規ユーザーエージェント作成'
+        centered
+        classNames={{ title: 'text-xl' }}
+      >
+        <UserAgentForm
+          onSubmit={async v => {
+            await onSubmit(v)
+            isOpen.setFalse()
+          }}
+          submitButtonName='作成する'
+        />
+      </Modal>
+    </>
+  )
+}
---
+++ frontend/src/features/userAgent/components/UpdateUserAgentButton.tsx
@@ -0,0 +1,37 @@
+import { Button, Modal } from '@mantine/core'
+import { FC } from 'react'
+
+import { useBoolean } from '@/hooks/useBoolean'
+
+import { UserAgentForm, UserAgentFormRequest } from './UserAgentForm'
+
+type Props = {
+  onSubmit: (params: UserAgentFormRequest) => void
+  initValue: UserAgentFormRequest
+}
+
+export const UpdateUserAgentButton: FC<Props> = ({ onSubmit, initValue }) => {
+  const isOpen = useBoolean()
+
+  return (
+    <>
+      <Button onClick={isOpen.setTrue}>編集</Button>
+      <Modal
+        opened={isOpen.v}
+        onClose={isOpen.setFalse}
+        title='カリキュラム編集'
+        centered
+        classNames={{ title: 'text-xl' }}
+      >
+        <UserAgentForm
+          onSubmit={async v => {
+            await onSubmit(v)
+            isOpen.setFalse()
+          }}
+          submitButtonName='更新する'
+          initValue={initValue}
+        />
+      </Modal>
+    </>
+  )
+}
