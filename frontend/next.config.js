aee3cd6ee4025f980fc0d269e590fabfd3e28b5d
akiogitgit <akimaron2828@icloud.com>
2023-05-25 12:01:44 +0900

    feat: MinIOに画像をアップロードして表示

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,4 +9,7 @@ module.exports = {
     return config
   },
   optimizeFonts: true,
+  images: {
+    domains: ['localhost'],
+  },
 }
---
+++ frontend/src/pages/minio.tsx
@@ -0,0 +1,86 @@
+import { NextPage } from 'next'
+// import Image from 'next/image'
+// import LegacyImage from 'next/legacy/image'
+import { ChangeEvent, useCallback, useState } from 'react'
+
+const Minio: NextPage = () => {
+  const [imageUrl, setImageUrl] = useState<string | undefined>()
+
+  const uploadFile = useCallback(
+    async (file: File, fileName: string, contentType: string) => {
+      console.log(file, fileName, contentType)
+
+      try {
+        // const res = await putApi(
+        //   `http://localhost:8003/aaaa/${fileName}`,
+        //   file,
+        //   {
+        //     'Content-Type': contentType,
+        //   },
+        // )
+        const res = await fetch(`http://localhost:8003/aaaa/${fileName}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': contentType,
+          },
+          body: file,
+        })
+        console.log('res: ', res)
+        setImageUrl(`http://localhost:8003/aaaa/${fileName}`)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    [],
+  )
+
+  const onChangeFile = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      console.log('file: ', e, e.target.files)
+      if (!e.target.files || e.target.files.length === 0) {
+        throw new Error('Please select the image file')
+      }
+
+      const file = e.target.files[0] // 1つでも配列になる
+      const fileExt = file.name.split('.').pop()
+      const fileName = `${Math.random()}.${fileExt}` // storageに保存する名前
+      const contentType = file.type
+
+      uploadFile(file, fileName, contentType)
+    },
+    [uploadFile],
+  )
+
+  return (
+    <>
+      <div>minoo</div>
+
+      <input type='file' onChange={onChangeFile} />
+
+      {imageUrl && <img src={imageUrl} alt='' />}
+      {/* <Image
+        src='https://source.unsplash.com/AwUIE3DdfGE'
+        width={400}
+        height={400}
+        alt=''
+      />
+
+      <Image
+        src='http://localhost:8003/aaaa/BlueHush.png'
+        width={400}
+        height={400}
+        alt=''
+      />
+      <LegacyImage
+        src='http://localhost:8003/aaaa/BlueHush.png'
+        width={400}
+        height={400}
+        alt=''
+      /> */}
+
+      {/* <img src='http://localhost:8003/aaaa/BlueHush.png' alt='' /> */}
+    </>
+  )
+}
+
+export default Minio
