82ef69a7c9639d8fd23b400942897498adcde65a
akiogitgit <akimaron2828@icloud.com>
2023-06-05 15:52:41 +0900

    feat: seedを更新

--- frontend/prisma/migrations/20230605064628_init/migration.sql
+++ frontend/prisma/migrations/20230605064628_init/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
 CREATE TABLE "Course" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     "description" TEXT NOT NULL,
     "level" INTEGER NOT NULL DEFAULT 1,
-    "imageUrl" TEXT,
+    "imageUrl" VARCHAR(255),
     "sectionIds" TEXT[],
-    "author" TEXT NOT NULL,
-    "organization" TEXT NOT NULL,
+    "author" VARCHAR(255) NOT NULL,
+    "organization" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -17,13 +17,13 @@ CREATE TABLE "Course" (
 -- CreateTable
 CREATE TABLE "Section" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
     "quizIds" TEXT[],
     "articleIds" TEXT[],
-    "scenarioGitHubUrl" TEXT,
-    "userAgentId" TEXT,
-    "courseId" TEXT NOT NULL,
+    "scenarioGitHubUrl" VARCHAR(255),
+    "userAgentId" VARCHAR(255),
+    "courseId" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -33,11 +33,11 @@ CREATE TABLE "Section" (
 -- CreateTable
 CREATE TABLE "Scenario" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     "description" TEXT NOT NULL,
-    "gitHubUrl" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "organization" TEXT NOT NULL,
+    "gitHubUrl" VARCHAR(255) NOT NULL,
+    "author" VARCHAR(255) NOT NULL,
+    "organization" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -48,7 +48,7 @@ CREATE TABLE "Scenario" (
 CREATE TABLE "Article" (
     "id" TEXT NOT NULL,
     "body" TEXT NOT NULL,
-    "sectionId" TEXT,
+    "sectionId" VARCHAR(255),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -60,9 +60,9 @@ CREATE TABLE "Quiz" (
     "id" TEXT NOT NULL,
     "question" TEXT NOT NULL,
     "choices" TEXT[],
-    "answer" TEXT NOT NULL,
+    "answer" VARCHAR(255) NOT NULL,
     "explanation" TEXT NOT NULL,
-    "sectionId" TEXT NOT NULL,
+    "sectionId" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -72,11 +72,11 @@ CREATE TABLE "Quiz" (
 -- CreateTable
 CREATE TABLE "UserAgent" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "gitHubUrl" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "organization" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "gitHubUrl" VARCHAR(255) NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "author" VARCHAR(255) NOT NULL,
+    "organization" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -84,16 +84,19 @@ CREATE TABLE "UserAgent" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Course_id_author_organization_key" ON "Course"("id", "author", "organization");
+CREATE UNIQUE INDEX "Course_name_author_organization_key" ON "Course"("name", "author", "organization");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Scenario_id_author_organization_key" ON "Scenario"("id", "author", "organization");
+CREATE UNIQUE INDEX "Section_name_courseId_key" ON "Section"("name", "courseId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "UserAgent_name_key" ON "UserAgent"("name");
+CREATE UNIQUE INDEX "Scenario_name_author_organization_key" ON "Scenario"("name", "author", "organization");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "UserAgent_id_author_organization_key" ON "UserAgent"("id", "author", "organization");
+CREATE UNIQUE INDEX "Quiz_question_sectionId_key" ON "Quiz"("question", "sectionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserAgent_name_author_organization_key" ON "UserAgent"("name", "author", "organization");
 
 -- AddForeignKey
 ALTER TABLE "Section" ADD CONSTRAINT "Section_userAgentId_fkey" FOREIGN KEY ("userAgentId") REFERENCES "UserAgent"("id") ON DELETE CASCADE ON UPDATE CASCADE;
