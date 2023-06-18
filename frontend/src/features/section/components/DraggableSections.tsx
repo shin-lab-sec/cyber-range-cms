1d19ad00eb40c8deb4b37e96a244129009c6293d
akiogitgit <akimaron2828@icloud.com>
2023-06-18 10:16:14 +0900

    feat: DraggableSectionsではSectionの順番変更だけにする

--- frontend/src/features/section/components/DraggableSections.tsx
+++ frontend/src/features/section/components/DraggableSections.tsx
@@ -1,5 +1,10 @@
 import { Button, Flex } from '@mantine/core'
-import { IconX, IconGripVertical } from '@tabler/icons-react'
+import {
+  IconArticle,
+  IconBox,
+  IconCurrencyQuetzal,
+  IconGripVertical,
+} from '@tabler/icons-react'
 import { FC, useEffect, useState } from 'react'
 import {
   DragDropContext,
@@ -8,24 +13,18 @@ import {
   DropResult,
 } from 'react-beautiful-dnd'
 
-import { SectionFormRequest } from './SectionForm'
-import { UpdateSectionButton } from './UpdateSectionButton'
 import { SectionWithUserAgent } from '../types'
 
 type Props = {
   sections: SectionWithUserAgent[]
   onUpdateOrder: (sections: SectionWithUserAgent[]) => void
-  onRemove: (id: string) => void
-  onUpdate: (id: string, v: SectionFormRequest) => void
-  className: string
+  className?: string
 }
 
 export const DraggableSections: FC<Props> = ({
   sections: sectionList,
   onUpdateOrder,
-  onRemove,
-  onUpdate,
-  className,
+  className = '',
 }) => {
   const [sections, setSections] = useState(sectionList)
 
@@ -49,13 +48,6 @@ export const DraggableSections: FC<Props> = ({
             {provided => (
               <ul {...provided.droppableProps} ref={provided.innerRef}>
                 {sections.map((section, index) => {
-                  // name, type, courseId, scenarioGitHubUrl, userAgentId
-                  const sectionFormRequest: SectionFormRequest = {
-                    name: section.name,
-                    type: section.type as 'quiz' | 'article' | 'sandbox',
-                    scenarioGitHubUrl: section.scenarioGitHubUrl ?? '',
-                    userAgentId: section.userAgentId ?? '',
-                  }
                   return (
                     <Draggable
                       key={section.id}
@@ -69,21 +61,18 @@ export const DraggableSections: FC<Props> = ({
                           ref={provided.innerRef}
                         >
                           <Flex align='center' gap='sm'>
-                            <IconGripVertical size={25} />
-                            <div className='rounded-md flex border-2 shadow-md mb-3 w-full py-4 px-4 items-center justify-between'>
-                              {section.name} type: {section.type}
-                              {/* <div>{JSON.stringify(section)}</div> */}
-                              <div className='flex gap-3 items-center'>
-                                <UpdateSectionButton
-                                  onSubmit={v => onUpdate(section.id, v)}
-                                  initValue={sectionFormRequest}
-                                />
-                                <IconX
-                                  size={44}
-                                  className='cursor-pointer mt-0.5 p-2.5'
-                                  onClick={() => onRemove(section.id)}
-                                />
-                              </div>
+                            <IconGripVertical size='1.5rem' />
+                            <div className='rounded-md flex border-2 shadow-md mb-3 w-full py-4 px-4 gap-3 items-center'>
+                              {section.type === 'quiz' && (
+                                <IconCurrencyQuetzal size='1.5rem' />
+                              )}
+                              {section.type === 'article' && (
+                                <IconArticle size='1.5rem' />
+                              )}
+                              {section.type === 'sandbox' && (
+                                <IconBox size='1.5rem' />
+                              )}
+                              {section.name}
                             </div>
                           </Flex>
                         </li>
