aabfb5547e4715ed744835ba7b31c2080e9c7cda
tosssssy <t.kabu.03@gmail.com>
2023-06-20 16:52:54 +0900

    参考用

--- frontend/src/pages/api/v1/useragents/index.ts
+++ frontend/src/pages/api/v1/useragents/index.ts
@@ -1,16 +1,40 @@
+import Cors from 'cors'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import prisma from '@/libs/prisma'
+
+const cors = Cors({
+  methods: ['GET'],
+  origin: 'https://cypas.sec',
+})
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function,
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  await runMiddleware(req, res, cors)
   // api/v1/useragents
   try {
     const userAgents = await prisma.userAgent.findMany({
       orderBy: { createdAt: 'asc' },
     })
+    console.log(userAgents)
     res.status(200).json({ data: userAgents })
   } catch (err) {
     res.status(400).json({ data: err })
