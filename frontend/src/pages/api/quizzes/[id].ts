2ba6646cd65564697d648b292eaec4dca9b6da6e
akiogitgit <akimaron2828@icloud.com>
2023-06-25 11:32:06 +0900

    feat: Quizスキーマ修正・API作成

---
+++ frontend/src/pages/api/quizzes/[id].ts
@@ -0,0 +1,62 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import prisma from '@/libs/prisma'
+import { apiValidation, quizSchema } from '@/libs/validates'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { method, body } = req
+  const id = String(req.query.id)
+
+  // articles/[id]
+  switch (method) {
+    case 'GET':
+      try {
+        const article = await prisma.quiz.findUnique({
+          where: {
+            id: id,
+          },
+        })
+        res.status(200).json({ data: article })
+      } catch (err) {
+        res.status(400).json({ data: err })
+      }
+      break
+
+    case 'PUT':
+      apiValidation(req, res, quizSchema, async () => {
+        const updatedArticle = await prisma.quiz.update({
+          where: {
+            id: id,
+          },
+          data: {
+            question: body.question,
+            choices: body.choices,
+            answers: body.answers,
+            explanation: body.explanation,
+          },
+        })
+        res.status(200).json({ data: updatedArticle })
+      })
+      break
+
+    case 'DELETE':
+      try {
+        const deletedArticle = await prisma.quiz.delete({
+          where: {
+            id: id,
+          },
+        })
+        res.status(200).json({ data: deletedArticle })
+      } catch (err) {
+        res.status(400).json({ data: err })
+      }
+      break
+
+    default:
+      res.status(405).end()
+      break
+  }
+}
---
+++ frontend/src/pages/api/quizzes/index.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import prisma from '@/libs/prisma'
+import { apiValidation, quizSchema } from '@/libs/validates'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { method, body } = req
+
+  // quizzes
+  switch (method) {
+    case 'GET':
+      try {
+        const quizzes = await prisma.quiz.findMany({
+          orderBy: { createdAt: 'asc' },
+        })
+        res.status(200).json({ data: quizzes })
+      } catch (err) {
+        res.status(400).json({ data: err })
+      }
+      break
+
+    case 'POST':
+      apiValidation(req, res, quizSchema, async () => {
+        const createdArticle = await prisma.quiz.create({
+          data: {
+            question: body.question,
+            type: body.type,
+            choices: body.choices,
+            answers: body.answers,
+            explanation: body.explanation,
+            section: { connect: { id: body.sectionId } },
+          },
+        })
+        res.status(200).json({ data: createdArticle })
+      })
+      break
+
+    default:
+      res.status(405).end()
+      break
+  }
+}
