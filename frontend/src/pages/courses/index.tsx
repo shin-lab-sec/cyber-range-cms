a4a730d5d55cc32f054f3140da0b50d752f3dbae
akiogitgit <akimaron2828@icloud.com>
2023-09-12 17:33:27 +0900

    feat: JSONデータをインポートして表示する

--- frontend/src/pages/courses/index.tsx
+++ frontend/src/pages/courses/index.tsx
@@ -19,12 +19,44 @@ const Courses: NextPage = () => {
   const { updateCourse } = useUpdateCourse()
   const { deleteCourse } = useDeleteCourse()
 
+  const [fileContent, setFileContent] = useState(null)
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      console.error('JSONファイルを選択して下さい')
+      return
+    }
+
+    const file = e.target.files[0]
+    if (!file) return
+
+    const reader = new FileReader()
+    reader.onload = event => {
+      const content = event?.target?.result
+      try {
+        const jsonData = JSON.parse(content as string)
+        setFileContent(jsonData)
+      } catch (error) {
+        console.error('JSONファイルを解析できませんでした。', error)
+      }
+    }
+    reader.readAsText(file)
+  }
+
   return (
     <Layout>
       <Flex gap='sm' justify='space-between' align='center'>
         <h1>コース一覧</h1>
         <CreateCourseButton onSubmit={createCourse} />
       </Flex>
+
+      <input type='file' accept='.json' onChange={handleFileChange} />
+      {fileContent && (
+        <div>
+          <h2>JSONデータ</h2>
+          <pre>{JSON.stringify(fileContent, null, 2)}</pre>
+        </div>
+      )}
 
       {courses?.length ? (
         <div className='mt-8'>
