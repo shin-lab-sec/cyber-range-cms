5ed8fbb372ce1fe92307464cce64bbc21706d7e9
akiogitgit <akimaron2828@icloud.com>
2023-09-15 16:41:04 +0900

    refactor: 複雑度を下げる

--- frontend/src/pages/api/courses/bulk.ts
+++ frontend/src/pages/api/courses/bulk.ts
@@ -5,196 +5,140 @@ import prisma from '@/libs/prisma'
 import { apiValidation, courseWithRelationSchema } from '@/libs/validates'
 import { SectionWithRelation } from '@/types'
 
+type CourseWithRelationRequest = Partial<Course> & {
+  sections?: { create: Partial<Section>[] }
+}
+
+type SectionWithRelationRequest = Partial<Section> & {
+  quizzes: { create: Partial<Quiz>[] }
+  articles: { create: Partial<Article>[] }
+  userAgent?: {
+    connectOrCreate: {
+      where: {
+        name_author_organization: Pick<
+          UserAgent,
+          'name' | 'author' | 'organization'
+        >
+      }
+      create: Partial<UserAgent>
+    }
+  }
+}
+
+// 既にUserAgentが存在すればconnect、無ければcreateする
+const generateUseragentRequest = (userAgent: UserAgent) => {
+  return {
+    userAgent: {
+      connectOrCreate: {
+        where: {
+          name_author_organization: {
+            name: userAgent.name,
+            author: userAgent.author,
+            organization: userAgent.organization,
+          },
+        },
+        create: {
+          name: userAgent.name,
+          gitHubUrl: userAgent.gitHubUrl,
+          type: userAgent.type,
+          author: userAgent.author,
+          organization: userAgent.organization,
+        },
+      },
+    },
+  }
+}
+
+const generateQuizzesRequest = (quizzes: Quiz[]) => {
+  return {
+    quizzes: {
+      create: quizzes.map(quiz => ({
+        question: quiz.question,
+        type: quiz.type,
+        choices: quiz.choices,
+        answers: quiz.answers,
+        explanation: quiz.explanation,
+      })),
+    },
+  }
+}
+
+const generateArticlesRequest = (articles: Article[]) => {
+  return {
+    articles: {
+      create: articles.map(article => ({
+        body: article.body,
+      })),
+    },
+  }
+}
+
+const generateSectionWithRelationRequest = (
+  sections: SectionWithRelation[],
+) => {
+  return sections.map(section => {
+    const quizzesRequest = generateQuizzesRequest(section.quizzes)
+    const articlesRequest = generateArticlesRequest(section.articles)
+    let userAgentRequest = {} // {}ならリクエストに含まれない
+
+    // userAgentがある時 既に存在すればconnect、無ければcreateする
+    if (section.userAgent) {
+      userAgentRequest = generateUseragentRequest(section.userAgent)
+    }
+
+    return {
+      name: section.name,
+      type: section.type,
+      scenarioGitHubUrl: section.scenarioGitHubUrl,
+      ...quizzesRequest,
+      ...articlesRequest,
+      ...userAgentRequest,
+    }
+  })
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { method, body } = req
+  const { body } = req
 
-  // api/courses
-  switch (method) {
-    case 'POST':
-      // OK bodyが返ってくる
-      // res.status(200).json({ data: body })
-      // return
+  // api/courses/bulk
+  apiValidation(req, res, courseWithRelationSchema, async () => {
+    let sectionsRequest = {}
 
-      // OK zod.parse通る
-      // apiValidation(req, res, courseRequestSchema, async () => {
-      //   res.status(200).json({ data: body })
-      // })
-      // return
+    // sectionがある時、sectionも同時に作成する
+    if (body.sections.length > 0) {
+      const sections = body.sections as SectionWithRelation[] // zodでparseしているのでSection[]でなければthrowする
+      const sectionsWithRelationRequest: SectionWithRelationRequest[] =
+        generateSectionWithRelationRequest(sections)
 
-      // OK sections: []のJSONで試す
-      // sectionのQuiz入れて試す
+      sectionsRequest = { sections: { create: sectionsWithRelationRequest } }
+    }
 
-      apiValidation(req, res, courseWithRelationSchema, async () => {
-        const courseWithRelationRequest: Partial<Course> & {
-          sections?: { create: Partial<Section>[] }
-        } = {
-          name: body.name,
-          description: body.description,
-          level: body.level,
-          imageUrl: body.imageUrl,
-          author: body.author,
-          organization: body.organization,
-          sectionIds: body.sectionIds,
-        }
+    const courseWithRelationRequest: CourseWithRelationRequest = {
+      name: body.name,
+      description: body.description,
+      level: body.level,
+      imageUrl: body.imageUrl,
+      author: body.author,
+      organization: body.organization,
+      sectionIds: body.sectionIds,
+      ...sectionsRequest, // {}ならリクエストに含まれない
+    }
 
-        // sectionsがある
-        if (body.sections.length > 0) {
-          const sections = body.sections as SectionWithRelation[] // zodでparseしているのでSection[]でなければthrowする
-
-          const sectionRequests: (Partial<Section> & {
-            quizzes: { create: Partial<Quiz>[] }
-            articles: { create: Partial<Article>[] }
-            userAgent?: {
-              connectOrCreate: {
-                where: {
-                  name_author_organization: Pick<
-                    UserAgent,
-                    'name' | 'author' | 'organization'
-                  >
-                }
-                create: Partial<UserAgent>
-              }
-            }
-          })[] = sections.map(section => {
-            let userAgent = {}
-
-            // userAgentがある時 存在すればconnect、無ければcreateする
-            if (section.userAgent) {
-              userAgent = {
-                userAgent: {
-                  connectOrCreate: {
-                    where: {
-                      name_author_organization: {
-                        name: section.userAgent.name,
-                        author: section.userAgent.author,
-                        organization: section.userAgent.organization,
-                      },
-                    },
-                    create: {
-                      name: section.userAgent.name,
-                      gitHubUrl: section.userAgent.gitHubUrl,
-                      type: section.userAgent.type,
-                      author: section.userAgent.author,
-                      organization: section.userAgent.organization,
-                    },
-                  },
-                },
-              }
-            }
-
-            return {
-              name: section.name,
-              type: section.type,
-              scenarioGitHubUrl: section.scenarioGitHubUrl,
-              quizzes: {
-                create: section.quizzes.map(quiz => ({
-                  question: quiz.question,
-                  type: quiz.type,
-                  choices: quiz.choices,
-                  answers: quiz.answers,
-                  explanation: quiz.explanation,
-                })),
-              },
-              articles: {
-                create: section.articles.map(article => ({
-                  body: article.body,
-                })),
-              },
-              // sandboxでない時は、userAgentを含めない
-              ...userAgent,
-            }
-          })
-
-          courseWithRelationRequest.sections = { create: sectionRequests }
-        }
-
-        const createdCourse = await prisma.course.create({
-          data: courseWithRelationRequest,
+    const createdCourse = await prisma.course.create({
+      data: courseWithRelationRequest as any, // courseWithRelationRequestに型を付けたかった
+      include: {
+        sections: {
           include: {
-            sections: {
-              include: {
-                userAgent: true,
-                articles: { orderBy: { createdAt: 'asc' } },
-                quizzes: { orderBy: { createdAt: 'asc' } },
-              },
-            },
+            userAgent: true,
+            articles: { orderBy: { createdAt: 'asc' } },
+            quizzes: { orderBy: { createdAt: 'asc' } },
           },
-        })
-        res.status(200).json({ data: createdCourse })
-      })
-      break
-
-    default:
-      break
-  }
+        },
+      },
+    })
+    res.status(200).json({ data: createdCourse })
+  })
 }
-
-// try {
-// const ress = await prisma.course.create({
-//   data: {
-//     name: '沢山作るコース',
-//     description: '沢山作るコース',
-//     level: 1,
-//     imageUrl: 'http://沢山作るコース',
-//     author: '沢山作るコース',
-//     organization: '沢山作るコース',
-//     sections: {
-//       create: [
-//         {
-//           name: 'quizだよん',
-//           type: 'quiz',
-//           quizzes: {
-//             create: [
-//               {
-//                 question: '問題1',
-//                 type: 'radio',
-//                 choices: ['選択肢1', '選択肢2', '選択肢3'],
-//                 answers: ['選択肢2'],
-//                 explanation: '解説',
-//               },
-//             ],
-//           },
-//         },
-//         {
-//           name: 'articleだよん',
-//           type: 'article',
-//           articles: {
-//             create: [{ body: '## anpan' }],
-//           },
-//         },
-//         {
-//           name: 'sandboxだよん',
-//           type: 'sandbox',
-//           // TODO: userAgentの作成で、同じの沢山出来て失敗しそう
-//           // 事前にname, organization, authorで検索してあれば、connectにする？
-//           // それか、名前に乱数追加して被らないようにする
-//           userAgent: {
-//             create: {
-//               name: 'ユーザーエージェントだよん',
-//               author: 'わし',
-//               organization: 'わし',
-//               type: 'わし',
-//               gitHubUrl:
-//                 'https://github.com/shin-lab-sec/cypas-cms/pull/84/files',
-//             },
-//           },
-//         },
-//       ],
-//     },
-//   },
-// })
-// res.status(200).json({ data: ress })
-// } catch (err) {
-//   if (err instanceof Error) {
-//     res.status(400).json({ message: err.message })
-//     return
-//   }
-
-//   res
-//     .status(400)
-//     .json({ message: `Invalid request: ${JSON.stringify(err)}` })
-// }
-// res.status(405).end()
