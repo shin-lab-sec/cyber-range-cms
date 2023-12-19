866e3a187498ddc7526b233bcf2c99b76b9d80da
akiogitgit <akimaron2828@icloud.com>
2023-12-19 13:51:50 +0900

    delete: basic認証を外す

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,24 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
+  // basic認証を外す
+  return
+
   // api/v1/下のリクエストは認証不要にする
-  if (req.nextUrl.pathname.includes('/api/v1')) return
+  // if (req.nextUrl.pathname.includes('/api/v1')) return
 
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [userName, password] = atob(authValue).split(':')
+  // if (basicAuth) {
+  //   const authValue = basicAuth.split(' ')[1]
+  //   const [userName, password] = atob(authValue).split(':')
 
-    if (
-      userName === process.env.BASIC_AUTH_NAME &&
-      password === process.env.BASIC_AUTH_PASSWORD
-    ) {
-      return NextResponse.next()
-    }
-  }
-  url.pathname = '/api/auth'
+  //   if (
+  //     userName === process.env.BASIC_AUTH_NAME &&
+  //     password === process.env.BASIC_AUTH_PASSWORD
+  //   ) {
+  //     return NextResponse.next()
+  //   }
+  // }
+  // url.pathname = '/api/auth'
 
-  return NextResponse.rewrite(url)
+  // return NextResponse.rewrite(url)
 }
