ca73904dc0d186c6d6642e38c4404f749794423b
akiogitgit <akimaron2828@icloud.com>
2024-01-16 10:31:40 +0900

    chore: src以外のコメント追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,16 +3,17 @@ const withTM = require('next-transpile-modules')([
   '@uiw/react-md-editor',
   '@uiw/react-markdown-preview',
 ])
+// WindiCSSのプラグイン
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 /** @type {import('next').NextConfig} */
 module.exports = withTM({
-  reactStrictMode: false,
-  swcMinify: true,
-  output: 'standalone',
-  optimizeFonts: true,
+  reactStrictMode: false, // devで起きる2度のレンダリングをさせない
+  swcMinify: true, // swcでminifyする
+  output: 'standalone', // ビルドしたときにindex.htmlを生成する
+  optimizeFonts: true, // フォントを最適化する
   images: {
-    domains: ['localhost', 'cms-storage.cypas.sec'],
+    domains: ['localhost', 'cms-storage.cypas.sec'], // NextImageで別ドメインの画像を許容する
   },
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin())
--- frontend/windi.config.js
+++ frontend/windi.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'windicss/helpers'
 
+// windicssの設定
 export default defineConfig({
   extract: {
-    include: ['**/*.{jsx,tsx,css}'],
-    exclude: ['node_modules', '.git', '.next'],
+    include: ['**/*.{jsx,tsx,css}'], // 対象ファイル
+    exclude: ['node_modules', '.git', '.next'], // 対象外ファイル
   },
   theme: {
     extend: {
       width: {
-        fit: 'fit-content',
+        fit: 'fit-content', // w-fitで指定できるようにする
       },
     },
   },
